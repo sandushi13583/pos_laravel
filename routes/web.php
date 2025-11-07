288d1410e70f52700d60bb345b6cf477a1bbb954
sandushi13583 <thilakaratnesandushi@gmail.com>
2025-11-07 10:18:51 +0530

    Redirect root URL to /login to skip install page in production

--- routes/web.php
+++ routes/web.php
@@ -76,7 +76,7 @@ include_once 'install_r.php';
 
 Route::middleware(['setData'])->group(function () {
     Route::get('/', function () {
-        return view('welcome');
+        return redirect('/login');
     });
 
     // Avoid directly calling Auth::routes() because on some deployments the
